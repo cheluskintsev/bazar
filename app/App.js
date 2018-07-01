cb5be50d1d7cf08aa9d5112cdfac7276ccd7ee36
cheluskintsev <cheluskintsev@icloud.com>
2018-07-01 15:12:37 +0200

    add navigator part 2

--- app/App.js
+++ app/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { AppRegistry, Text, View } from "react-native";
+import Navigator from "./Navigator";
 
 class App extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text style={{ fontSize: 100 }}>HI</Text>
+      <View style={{ flex: 1 }}>
+        {/* <Text style={{ fontSize: 100 }}>HI</Text> */}
+        <Navigator />
       </View>
     );
   }
--- app/Navigator.js
+++ app/Navigator.js
@@ -1,9 +1,74 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Platform, StatusBar } from "react-native";
+import { Icon } from "react-native-elements";
+import {
+  createBottomTabNavigator,
+  createStackNavigator
+} from "react-navigation";
+import StoreView from "./StoreView";
+import SearchView from "./SearchView";
+import FavoritesView from "./FavoritesView";
+import ProfileView from "./ProfileView";
+import SettingsView from "./SettingsView";
+
+const BottomNavigator = createBottomTabNavigator(
+  {
+    Store: { screen: StoreView },
+    Search: { screen: SearchView },
+    Favorites: { screen: FavoritesView },
+    Profile: { screen: ProfileView }
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: () => {
+        const { routeName } = navigation.state;
+        let iconName;
+        switch (routeName) {
+          case "Store":
+            return (
+              <Icon type="font-awesome" name="shopping-bag" color="black" />
+            );
+          case "Search":
+            return <Icon type="font-awesome" name="search" color="black" />;
+          case "Favorites":
+            return <Icon type="font-awesome" name="heart" color="black" />;
+          case "Profile":
+            return <Icon type="font-awesome" name="user" color="black" />;
+          default:
+            null;
+        }
+        return <Icon type="font-awesome" name={iconName} color="black" />;
+      },
+      tabBarOptions: {
+        showLabel: false
+      }
+    })
+  }
+);
+
+export const AppStack = createStackNavigator(
+  {
+    Main: {
+      screen: BottomNavigator
+    },
+    Settings: { screen: SettingsView }
+  },
+  {
+    mode: "card",
+    headerMode: Platform.OS === "ios" ? "float" : "screen",
+    initialRouteName: "Main"
+  }
+);
 
 class Navigator extends React.Component {
   render() {
-    return <View />;
+    return (
+      <View style={{ flex: 1 }}>
+        {/* <StatusBar /> */}
+        <AppStack />
+        {/* <BottomTabNavigator /> */}
+      </View>
+    );
   }
 }
 
